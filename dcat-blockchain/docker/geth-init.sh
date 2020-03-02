26b3fb3be099431cd09dfa060f463d2b988844a5
root <root@WSCCI000.umhs.med.umich.edu>
2020-03-02 17:12:01 -0500

    docker update

--- dcat-blockchain/docker/geth-init.sh
+++ dcat-blockchain/docker/geth-init.sh
@@ -4,12 +4,12 @@ geth --exec "admin.addPeer('enode://627de6fb3896802aaa6e119cb83c1dab4ff9417574b0
 cd /dcnet/node1
 npm -y install web3
 echo "Wating for connection..."
-sleep 60
-echo "Getting data.."
-sleep 60
-echo "Getting data..."
-sleep 60
-echo "Getting data...."
-sleep 60
-echo "Generating file..."
+sleep 30
+echo "Getting data..1"
+sleep 30
+echo "Getting data...2"
+sleep 30
+echo "Getting data....3"
+sleep 30
+echo "Generating file...4"
 node get-data.js
