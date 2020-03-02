0a38f6061094adab0cdb07887c28dd9fe22637b7
Wei Shi <shiwei@ltit0069.wifi.med.umich.edu>
2020-03-02 11:59:23 -0500

    correction

--- dcat-blockchain/docker/create-content.sh
+++ dcat-blockchain/docker/create-content.sh
@@ -1,8 +1,8 @@
 docker stop $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker-compose up -d poqianggo
-#sleep 10
+sleep 3
 ##docker-compose up dkr
-#docker exec $(docker ps -q) /bin/bash /dcnet/node1/geth-init.sh
-docker exec -ti $(docker ps -q) /bin/bash
-#docker stop $(docker ps -q)
+docker exec $(docker ps -q) /bin/bash /dcnet/node1/geth-init.sh
+#docker exec -ti $(docker ps -q) /bin/bash
+docker stop $(docker ps -q)
